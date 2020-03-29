572d1b4dbc20344e7807b92109c13c07e590cf41
HaQadosch Berraka <haqadosch@pm.me>
2020-03-29 22:05:53 +0100

    Updates

---
+++ precache-manifest.8ab94dab7209ccff1be2724a53c8704f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a8c99b53242f721c29018133dbb789a",
+    "url": "/frontend-dev-task/index.html"
+  },
+  {
+    "revision": "aa29341001bd8c97ef3b",
+    "url": "/frontend-dev-task/static/css/2.2fbe55bd.chunk.css"
+  },
+  {
+    "revision": "5f6e7a783a71014b30e4",
+    "url": "/frontend-dev-task/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "aa29341001bd8c97ef3b",
+    "url": "/frontend-dev-task/static/js/2.9526fab0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/frontend-dev-task/static/js/2.9526fab0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f6e7a783a71014b30e4",
+    "url": "/frontend-dev-task/static/js/main.fc1824e8.chunk.js"
+  },
+  {
+    "revision": "6d926a81b31cad8994e4",
+    "url": "/frontend-dev-task/static/js/runtime-main.49eb033e.js"
+  }
+]);
