553021a81b242a781142583fe4a996d5e00d681c
HaQadosch Berraka <haqadosch@pm.me>
2020-04-02 23:42:41 +0100

    css: style the cards in a separate css

--- instagroom/src/ui/BroomCard.tsx
+++ instagroom/src/ui/BroomCard.tsx
@@ -1,22 +1,30 @@
-import React from "react";
+import React from "react"
+import './BroomCard.css'
+
 import ErrorBoundary, { FallbackProps } from 'react-error-boundary'
 
-import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
-import { Card, Avatar } from 'antd';
-const { Meta } = Card;
+import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons'
+import { Card, Avatar } from 'antd'
+const { Meta } = Card
 
 interface IBroomCardProps {
   item: any
 }
 
+const referals = '?utm_source=instagroom&utm_medium=referral'
+
 const BroomCard: React.FC<IBroomCardProps> = ({ item }) => {
 
   return (
     <Card
+      className='card'
+      hoverable
       style={ {
-        width: 300,
         backgroundColor: `${ item.color || '#000000' }87`
       } }
+      // extra={
+      //   <Avatar src={ item.user.profile_image.small } alt={ item.user.name } />
+      // }
       cover={
         <img
           alt={ item.alt_description }
@@ -31,12 +39,22 @@ const BroomCard: React.FC<IBroomCardProps> = ({ item }) => {
     >
       <Meta
         avatar={
-          <a href={ item.user.links.html } >
+          <a href={ `${ item.user.links.html }/${ referals }` } >
             <Avatar src={ item.user.profile_image.small } alt={ item.user.name } />
           </a>
         }
-        title={ <a href={ item.links.html } style={ { color: 'black' } }>{ item.description || 'no title' }</a> }
-        description={ item.alt_description }
+        title={ <a className='card__meta card__title' href={ `${ item.links.html }/${ referals }` } >{ item.description || 'no title' }</a> }
+        description={
+          <>
+            <p className='card__meta card__despcription'>
+              Photo by
+              <a href={ `${ item.user.links.html }/${ referals }` }>{ item.user.name }</a>
+              on
+              <a href={ `https://unsplash.com/${ referals }` }>Unsplash</a>
+            </p>
+            { item.alt_description }
+          </>
+        }
       />
     </Card>
   )
